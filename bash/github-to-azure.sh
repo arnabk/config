2ecaab94120ef967c8d32936d32db8b8c8e3fb13
Arnab Karmakar <arnab.k@gmail.com>
2019-05-05 17:49:44 -0600

    Add script for azure migration

---
+++ bash/github-to-azure.sh
@@ -0,0 +1,13 @@
+#  This script assumes that ssh-add ~/.ssh/id has been called running the following commands
+GITHUB_USER=arnabk
+AZURE_USER=arnabk
+
+REPOSITORY_NAME=$1
+
+git clone git@github.com:$GITHUB_USER/$REPOSITORY_NAME.git
+cd $REPOSITORY_NAME
+git remote add upstream git@ssh.dev.azure.com:$AZURE_USER/$REPOSITORY_NAME
+git push upstream master
+git push --tags upstream
+cd ..
+rm -fR $REPOSITORY_NAME%
